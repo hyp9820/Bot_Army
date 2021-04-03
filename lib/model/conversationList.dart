8eb57ddbb3916b91bb341c78c3c05b176cf5a236
Anushka Darade <anushkadarade@gmail.com>
2021-04-03 19:47:03 +0530

    Changes to Conversation Page

--- lib/model/conversationList.dart
+++ lib/model/conversationList.dart
@@ -1,49 +1,4 @@
 List conversationList = [
-<<<<<<< HEAD
-    {
-      "name": "Novac",
-      "imageUrl": "https://randomuser.me/api/portraits/men/31.jpg",
-      "isOnline": true,
-      "message": "Where are you?",
-      "time": "5:00 pm"
-    },
-    {
-      "name": "Derick",
-      "imageUrl": "https://randomuser.me/api/portraits/men/81.jpg",
-      "isOnline": false,
-      "message": "These medications are good!!",
-      "time": "7:00 am"
-    },
-    {
-      "name": "Emannual",
-      "imageUrl": "https://randomuser.me/api/portraits/men/35.jpg",
-      "isOnline": true,
-      "message": "Got to go!! Bye!!",
-      "time": "yesterday"
-    },
-    {
-      "name": "Gracy",
-      "imageUrl": "https://randomuser.me/api/portraits/women/56.jpg",
-      "isOnline": false,
-      "message": "Sorry, I forgot!",
-      "time": "2nd Feb"
-    },
-    {
-      "name": "Robert",
-      "imageUrl": "https://randomuser.me/api/portraits/men/36.jpg",
-      "isOnline": false,
-      "message": "No, I won't go!",
-      "time": "28th Jan"
-    },
-    {
-      "name": "Emma",
-      "imageUrl": "https://randomuser.me/api/portraits/women/56.jpg",
-      "isOnline": false,
-      "message": "How are you feeling?",
-      "time": "15th Jan"
-    }
-  ];
-=======
   {
     "name": "Novac",
     "imageUrl": "https://randomuser.me/api/portraits/men/31.jpg",
@@ -87,4 +42,3 @@ List conversationList = [
     "time": "15th Jan"
   }
 ];
->>>>>>> d665b144c0cc5839f5b32d76f97c516485ae4d54
